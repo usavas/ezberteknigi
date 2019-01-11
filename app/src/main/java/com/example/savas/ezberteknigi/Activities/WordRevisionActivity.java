59b81fbfd98da811f9db0b29e5fe435839a693aa
savas <savasusame@gmail.com>
2019-01-11 15:31:20 +0300

    WordRevisionActivity added (WordLearningActivity used as fragment) not implemented yet

---
+++ app/src/main/java/com/example/savas/ezberteknigi/Activities/WordRevisionActivity.java
@@ -0,0 +1,30 @@
+package com.example.savas.ezberteknigi.Activities;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.savas.ezberteknigi.Models.Word;
+import com.example.savas.ezberteknigi.R;
+
+public class WordRevisionActivity extends AppCompatActivity
+implements WordLearningFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_revision_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, WordLearningFragment.newInstance(Word.WORD_REVISION))
+                    .commitNow();
+        }
+    }
+
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
