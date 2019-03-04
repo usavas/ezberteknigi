c4c465a063807fa03fa26d801fa2f4b1aafef3d7
savas <savasusame@gmail.com>
2019-03-04 21:15:06 +0300

    layout for books and book item added

---
+++ app/src/main/java/com/example/savas/ezberteknigi/Activities/SearchBooksActivity.java
@@ -0,0 +1,15 @@
+package com.example.savas.ezberteknigi.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.savas.ezberteknigi.R;
+
+public class SearchBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_books);
+    }
+}
