bba5e24029c80b05be0abfe0f743189b3967df6a
savas <savasusame@gmail.com>
2019-03-11 16:47:30 +0300

    appstarter class and notification channels create

---
+++ app/src/main/java/com/example/savas/ezberteknigi/AppStarter.java
@@ -0,0 +1,41 @@
+package com.example.savas.ezberteknigi;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class AppStarter extends Application {
+
+    public static final String CHANNEL_WORD_REVISION = "CHANNEL_WORD_REVISION";
+    public static final String CHANNEL_WORD_OF_THE_DAY = "CHANNEL_WORD_OF_THE_DAY";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channelWordRevision = new NotificationChannel(
+                    CHANNEL_WORD_REVISION,
+                    "Kelime Tekrarı",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channelWordRevision.setDescription("Periyodik olarak tekrar edilmesi öngörülen kelimeler için bildirimde bulunur.");
+
+            NotificationChannel channelWordOftheDay = new NotificationChannel(
+                    CHANNEL_WORD_OF_THE_DAY,
+                    "Günün Kelimesi",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channelWordOftheDay.setDescription("Her gün sistem tarafından gönderilecek bir İngilizce kelimeyi bildirim olarak görüntüler.");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channelWordRevision);
+            manager.createNotificationChannel(channelWordOftheDay);
+        }
+    }
+}
